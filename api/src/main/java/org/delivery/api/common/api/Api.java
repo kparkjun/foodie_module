6426de6012336c17076a3f57bfaf00e7e05eddf1
kparkjun <aparkjun@naver.com>
2025-03-02 09:24:34 +0900

    -프로젝트 베이스 개발 - Api Error Code 적용하기

--- api/src/main/java/org/delivery/api/common/api/Api.java
+++ api/src/main/java/org/delivery/api/common/api/Api.java
@@ -4,6 +4,7 @@ import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.delivery.api.common.error.ErrorCodeIfs;
 
 @Data
 @NoArgsConstructor
@@ -21,4 +22,29 @@ public class Api<T> {
         api.body=data;
         return api;
     }
+
+    public static Api<Object> ERROR(Result result){
+        var api=new Api<Object>();
+        api.result=result;
+        return api;
+    }
+
+    public static Api<Object> ERROR(ErrorCodeIfs errorCodeIfs){
+        var api=new Api<Object>();
+        api.result=Result.ERROR(errorCodeIfs);
+        return api;
+    }
+
+    public static Api<Object> ERROR(ErrorCodeIfs errorCodeIfs,Throwable tx){
+        var api=new Api<Object>();
+        api.result=Result.ERROR(errorCodeIfs,tx);
+        return api;
+    }
+
+    public static Api<Object> ERROR(ErrorCodeIfs errorCodeIfs,String description){
+        var api=new Api<Object>();
+        api.result=Result.ERROR(errorCodeIfs,description);
+        return api;
+    }
+
 }
