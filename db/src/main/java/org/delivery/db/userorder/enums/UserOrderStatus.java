a4c6c484600937bf12087a2ca5e2a8c6bcc3f5b2
kparkjun <aparkjun@naver.com>
2025-03-13 00:20:45 +0900

    -상품 주문 개뱔- User Order 서비스 로직 개발 -2

--- db/src/main/java/org/delivery/db/userorder/enums/UserOrderStatus.java
+++ db/src/main/java/org/delivery/db/userorder/enums/UserOrderStatus.java
@@ -5,7 +5,24 @@ public enum UserOrderStatus {
     REGISTERED("등록"),
 
     UNREGISTERED("해지"),
+
+    //주문
+    ORDER("주문"),
+    ACCEPT("확인"),
+    COOKING("요리중"),
+    DELIVERY("배달중"),
+    RECEIVE("완료")
     ;
+    //확인
+
+    //요리중
+
+    //배달중
+
+    //완료
+
+
+
 
     UserOrderStatus(String description){
         this.description=description;
